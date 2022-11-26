f388ced03d360cb246630419d4c4396cc6d3aba0
CKS <communicate2charles@gmail.com>
2022-11-26 14:28:22 +0700

    solved

--- 557. Reverse Words in a String III.py
+++ 557. Reverse Words in a String III.py
@@ -1,3 +1,14 @@
 """Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 
 """
+s = "Let's take LeetCode contest"
+
+
+def reverseWords(s: str) -> str:
+    return ' '.join(token[::-1] for token in s.split())
+    #     fastes:
+    # return " ".join(s.split()[::-1])[::-1]
+
+print(reverseWords(s))
+
+
