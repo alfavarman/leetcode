de7bec859e0f5bfee2a1567ccca9a43578a80a0a
CKS <communicate2charles@gmail.com>
2022-11-26 13:48:36 +0700

    move zeros

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -8,14 +8,13 @@ def moveZeroes(nums: list[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
-    for i in range(len(nums)):
-        if nums[i] == 0:
-            nums.insert(-1, nums.pop(i))
+    for a in range(len(nums)-2, -1, -1):
+        if nums[a] == 0:
+            nums.append(nums.pop(a))
 
 
 
 
-nums = [0,1,0,3,12]
-print(nums)
+nums = [0,0,1,3,12]
 moveZeroes(nums)
 print(nums)
